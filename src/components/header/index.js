1aa2741a72d3edbca866b2781d1ccba0393b9f65
Alexander Klapauch <aklapauch@MIN-MB-000.local>
2020-01-22 23:37:19 -0500

    add tests

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,8 +5,8 @@ import Logo from '../../assets/graphics/logo.png';
 const Header = (props) => {
     return (
         <header>
-            <div className="wrap">
-                <div className="logo">
+            <div className="wrap" data-test="headerComponent">
+                <div className="logo" data-test="logoIMG">
                     <img src={Logo} alt="Logo" />
                 </div>
             </div>
---
+++ src/components/headline/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Headline extends Component {
+    render() {
+        const { header, desc } = this.props;
+        if (!header) {
+            return null;
+        }
+
+        return (
+            <div data-test="HeadlineComponent">
+                <h1 data-test="header">{header}</h1>
+                <p data-test="description">
+                    {desc}
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Headline;
